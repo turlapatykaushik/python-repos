123f15d505f48ec5b1e636ee265f5db579fa3218
turlapatykaushik <turlapatykaushik95@gmail.com>
2015-07-25 14:33:12 +0530

    Access data Added

---
+++ access_data.py
@@ -0,0 +1,8 @@
+import urllib2
+def main():
+    weburl = urllib2.urlopen("https://www.github.com/turlapatykaushik")
+    print str(weburl.getcode())
+    data = weburl.read()
+    print data
+if __name__ == "__main__":
+    main()
