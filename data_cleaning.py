cb01a2718f4dff60362f3e8dfcb08ea592398710
turlapatykaushik <turlapatykaushik95@gmail.com>
2015-07-26 20:50:26 +0530

    data_cleaning.py added

---
+++ data_cleaning.py
@@ -0,0 +1,5 @@
+tweet = " I like my &lt;3 phone &amp; you're awesome sony. Display Is Terrific , soo happyy :) http://www.sony.com"
+import HTMLParser
+html_parser = HTMLParser.HTMLParser()
+newtweet = html_parser.unescape(tweet)
+print newtweet
