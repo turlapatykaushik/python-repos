d9b442c420183cdbc406ef883e198473d12295fd
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-07-26 13:24:08 +0530

    Update search_file.py

--- search_file.py
+++ search_file.py
@@ -1,3 +1,4 @@
+// A script to search the system for a file . Just replace the filename variable in the code with the actual filename of what you want to search
 import os
 from os.path import join
 
