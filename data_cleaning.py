150d024153a0aafebe210a1bbef0abdc1cbe69df
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-07-26 20:53:08 +0530

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,6 @@
 //Text Data cleaning script in python
 // Escaping HTML Characters
+
 tweet = " I like my &lt;3 phone &amp; you're awesome sony. Display Is Terrific , soo happyy :) http://www.sony.com"
 import HTMLParser
 html_parser = HTMLParser.HTMLParser()
